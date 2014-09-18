dbe06db5eab31f30127eac1e79e1cac564e43d01
jt222ii <jt222ii@student.lnu.se>
2014-09-18 16:16:18 +0200

    Started the laboration

---
+++ Labb3Lonerevision/Labb3Lonerevision/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3Lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Labb3Lonerevision/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3Lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadInt();
+        }
+        static int ReadInt()
+        {
+            while (true)
+            {
+                try
+                {
+                    int Salaries = int.Parse(Console.ReadLine());
+                    int[] SalariesArray;
+                    SalariesArray = new int[Salaries];
+                }
+                catch
+                {
+                    Console.WriteLine("Ogiltig summa. Var god försök igen");
+                }
+            }
+        }
+        static void ProcessSalaries(int count)
+        { 
+        
+        }
+
+    }
+}
