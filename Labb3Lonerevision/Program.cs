ff926874c9deeefe4efa5e59ee365f00256890fa
jt222ii <jt222ii@student.lnu.se>
2014-09-20 17:10:30 +0200

    Finished MOST of the task. Still need to failcheck it!

--- Labb3Lonerevision/Program.cs
+++ Labb3Lonerevision/Program.cs
@@ -10,27 +10,101 @@ namespace Labb3Lonerevision
     {
         static void Main(string[] args)
         {
-            ReadInt();
+            do
+            {
+                Console.Clear();
+                int amountOfSalaries = ReadInt("Ange antal löner att mata in: ");
+                Console.WriteLine();
+                if (amountOfSalaries < 2)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Antal löner måste vara 2 eller mer!");
+                    Console.ResetColor();
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Tryck valfri tangent för ny uträkning - ESC avslutar ");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    ProcessSalaries(amountOfSalaries);
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
-        static int ReadInt()
+        static int ReadInt(string prompt)
         {
+
             while (true)
             {
                 try
                 {
-                    int Salaries = int.Parse(Console.ReadLine());
-                    int[] SalariesArray;
-                    SalariesArray = new int[Salaries];
+                    Console.Write(prompt);
+                    int amountOfSalaries = int.Parse(Console.ReadLine());
+                    return amountOfSalaries;
                 }
                 catch
                 {
+                    Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("Ogiltig summa. Var god försök igen");
+                    Console.ResetColor();
                 }
             }
         }
         static void ProcessSalaries(int count)
-        { 
-        
+        {
+            int[] salaries = new int[count];
+            int[] sortedSalaries = new int[count];  
+
+            for (int i = 0; i < count; i++)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Ange lön nummer {0,10}: ", i + 1);
+                        salaries[i] = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ogiltig summa. Var god försök igen");
+                        Console.ResetColor();
+                    }
+                }
+            }
+            sortedSalaries = (int[])salaries.Clone();
+            Array.Sort(sortedSalaries);
+            int median;
+            if (count % 2 == 0)
+            {
+                int number1 = sortedSalaries.Length / 2;
+                int number2 = number1 - 1;
+                median = (sortedSalaries[number1] + sortedSalaries[number2])/2;
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Medianlön: {0,10:C0}", median);
+            }
+            else
+            {
+                median = sortedSalaries.Length / 2;
+                Console.WriteLine("Medianlön:     {0,10:C0}", sortedSalaries[median]);
+            }
+
+            Console.WriteLine("Medellön:      {0,10:C0}", sortedSalaries.Average());
+            Console.WriteLine("Lönespridning: {0,10:C0}",sortedSalaries.Max() - sortedSalaries.Min());
+            Console.WriteLine("---------------------------------");
+            for (int i = 0; i < count; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+                Console.Write("{0,10}", salaries[i]);
+            }
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\nTryck valfri tangent för ny uträkning - ESC avslutar ");
+            Console.ResetColor();
         }
 
     }
